4c0055016747632b2d99a51b5d68f95f50777357
Fraxtal <nicholaspangts@gmail.com>
2024-02-15 10:09:15 +0800

    Initial  commit

--- main.py
+++ main.py
@@ -1,34 +1,19 @@
 import discord
+from discord.ext import commands
 from settings import token
-from commands import ping
 
-bot = discord.Bot()
+client = commands.Bot(command_prefix=os.getenv("command_prefix"), case_insensitive=True)
 
-# @bot.event
-# async def on_ready():
-#     print(f"Logged into discord api as: {bot.user}")
-
-# @bot.slash_command()
-# async def hello(ctx, name: str = None):
-#     name = name or ctx.author.name	
-#     await ctx.respond(f"Hello {name}!")
-
-# @bot.user_command(name="Say Hello")
-# async def hi(ctx, user):
-#     await ctx.respond(f"{ctx.author.mention} says hello to {user.name}!")
-
-@bot.event
+@client.event
 async def on_connect():
     print(f"{bot.user} is ready and online!")
 
-@bot.slash_command(name = "hello", description = "Say hello to the bot")
+@client.slash_command(name = "hello", description = "Say hello to the bot")
 async def hello(ctx):
     await ctx.respond(f"Hey {ctx.author.mention}!")
     
-@bot.slash_command()
+@client.slash_command()
 async def say(ctx, 	message = "What do you want me to say?"):
     await ctx.send(f'{message}')
 
-
-bot.add_command(ping)
-bot.run(token)
+client.run(token)
